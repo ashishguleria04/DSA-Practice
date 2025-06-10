8556dd2096e07019db8505eb19afe9ad86492c50
Ashish <110624199+ashishguleria04@users.noreply.github.com>
2025-06-10 10:50:04 +0530

    Time: 0 ms (100%), Space: 13.5 MB (39.77%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -14,13 +14,13 @@ public:
         ListNode* prev = nullptr;
         ListNode* curr = head;
 
-        while (curr != nullptr) {
+
+        while (curr != nullptr){
             ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-
         return prev;
     }
 };
